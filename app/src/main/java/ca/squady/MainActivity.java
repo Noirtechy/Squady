2a29e79b4cbc435ff639e78e0e5300b40224483e
Noirtechy <35749282+Noirtechy@users.noreply.github.com>
2018-01-26 01:26:39 -0500

    Revert "Ife david"

--- app/src/main/java/ca/squady/MainActivity.java
+++ app/src/main/java/ca/squady/MainActivity.java
@@ -2,18 +2,12 @@ package ca.squady;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        //New Changes
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
-
 }

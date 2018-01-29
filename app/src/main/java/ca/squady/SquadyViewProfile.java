6d6db4766ee83264434c3d7306f0c6fc6000f8c9
Gozie <dexterlegion1@gmail.com>
2018-01-29 00:23:14 -0500

    navigation drawer for view profile

--- app/src/main/java/ca/squady/SquadyViewProfile.java
+++ app/src/main/java/ca/squady/SquadyViewProfile.java
@@ -1,18 +1,35 @@
 package ca.squady;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class SquadyViewProfile extends AppCompatActivity
 {
+    private DrawerLayout drawer;
+   Toolbar toolbar_top;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.squady_viewprofile);
+
+        //actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.textColor));
+        toolbar_top = (Toolbar) findViewById(R.id.toolbar_top);
+        setSupportActionBar(toolbar_top);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar_top, R.string.open, R.string.close);
+        drawer.setDrawerListener(actionBarDrawerToggle);
 
         final TextView profileusername = (TextView) findViewById(R.id.usernameHeader);
         final TextView profilename = (TextView) findViewById(R.id.profilename);
@@ -26,6 +43,13 @@ public class SquadyViewProfile extends AppCompatActivity
         profilename.setText(user.getName());
         profileemail.setText(user.getEmail());
         profilephonenumber.setText(user.getPhonenumber());
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
     }
 
     public void editProfile (View view)
